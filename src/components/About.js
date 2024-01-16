3b786948d7c79c9ca27c4d96c9ffb65a966f028a
nischay-code <nischay.patel.code@gmail.com>
2024-01-16 19:03:40 +0530

    Modification done 🚀

--- src/components/About.js
+++ src/components/About.js
@@ -2,13 +2,20 @@ import React from "react";
 
 export const About = () => {
   return (
-    <div>
-      This is an about component
+    <div className="container my-3">
+      <h2>This is an about component</h2>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum corporis
-        odio, nesciunt sed eveniet, nemo amet, dignissimos magnam itaque ipsam
-        possimus. Inventore vitae, perferendis harum quas cupiditate adipisci
-        repellendus tempore asperiores at consequatur quos?
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex corporis
+        nulla fugiat consequatur quibusdam! Quae illum autem excepturi ex magni
+        est libero praesentium officiis rerum sequi, sit voluptatum deleniti
+        ullam laboriosam saepe. Veniam eveniet iusto nesciunt a quas velit vel
+        praesentium labore autem voluptatem illo atque beatae assumenda cum
+        veritatis temporibus alias culpa nemo reiciendis dolore facere, eligendi
+        doloribus fuga inventore! Consequuntur excepturi pariatur deleniti
+        quisquam esse possimus obcaecati, sapiente fuga consectetur nemo rerum
+        eveniet provident aut veniam eius impedit. Velit ipsa, blanditiis
+        aliquid a similique consequuntur enim commodi. Ea repellat voluptatum
+        maiores ducimus, cupiditate quos expedita possimus doloremque nesciunt.
       </p>
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <footer className="bg-dark text-light py-3">
+    <footer className="fixed-bottom bg-dark text-light py-2">
       <p className="text-center">Copyright &copy; MyTodosList.com</p>
     </footer>
   );
